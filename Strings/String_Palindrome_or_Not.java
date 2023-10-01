8519c2adf9575428132ae807bf5451110f01f38a
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-01 22:07:17 +0530

    Add files via upload

---
+++ Strings/String_Palindrome_or_Not.java
@@ -0,0 +1,27 @@
+package com.kn.daily_practice;
+
+import java.util.Scanner;
+
+public class String_Palindrome_or_Not {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a text : ");
+		String s1 = scan.next();
+
+		char[] arr = s1.toCharArray();
+		String reverse = "";
+
+		scan.close();
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			reverse = reverse + arr[i];
+		}
+		System.out.println(reverse);
+
+		if (s1.equals(reverse)) {
+			System.out.println("PALINDROME");
+		} else {
+			System.out.println("NOT PALINDROME");
+		}
+	}
+}
