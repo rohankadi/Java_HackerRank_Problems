65c99cec9e245125274f886754b984b1354b7110
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-27 10:15:41 +0530

    Add files via upload

---
+++ DSA/ArrayToString.java
@@ -0,0 +1,13 @@
+package com.kn.dsa;
+
+public class ArrayToString {
+
+	public static void main(String[] args) {
+		char[] arr = { 'j', 'a', 'v', 'a' };
+
+		String str = new String(arr);
+
+		System.out.println(str);
+	}
+
+}
